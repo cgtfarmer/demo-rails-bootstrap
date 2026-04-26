767f8024247cdc94429b91ed373dc87f24c8878a
Christian Farmer <christian@taxstrategistsofamerica.com>
2026-04-26 16:50:20 -0500

    Finish basic functionality

--- Dockerfile
+++ Dockerfile
@@ -29,6 +29,7 @@ ENTRYPOINT ["/rails/bin/docker-entrypoint"]
 
 # Start the server by default, this can be overwritten at runtime
 EXPOSE 3000
+
 CMD ["./bin/rails", "server", "-b", "0.0.0.0"]
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,34 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+
+    redirect_to users_path
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :age, :weight, :smoker)
+  end
 end
